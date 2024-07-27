f2d619f6bf807c92a78ad8a289516a28df1361b2
LazerThings <ibananiza@icloud.com>
2024-07-27 18:13:14 -0500

    Initial Commit

---
+++ Claude Desktop App/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Claude Desktop App
+//
+//  Created by Gibson Drake Reiff on 7/27/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
